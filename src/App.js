94d7a953a0b417ac4fa1ee320c2fa20f795642aa
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-04-12 15:24:05 -0400

    stock array added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./style.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main"
 import About from "./pages/about";
 import Stock from "./pages/stock";
 import Dashboard from "./pages/dashboard";
+import stocks from "./data";
 
 function App() {
     return (
@@ -11,7 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/stocks/:symbol" element={<Stock />} />
+                <Route path="/data/:symbol" element={<Stock />} />
                 <Route path="/stocks" element={<Dashboard />} />
             </Routes>
         </div>
