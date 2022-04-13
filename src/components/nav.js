c1a518c5be30fe95bda9289e86d9f17d79575040
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-04-12 21:17:08 -0400

    nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Nav(props) {
     return <nav aria-label="Main Navigation" role="navigation">
         <ul>
-            <li><Link to='./'>Homepage</Link></li>
+            <li><Link to='./main'>Homepage</Link></li>
             <li><Link to='./stocks'>Dashboard</Link></li>
             <li><Link to='./about'>About</Link></li>
         </ul>
